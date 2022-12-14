95fe5f9e3ed04350bf722cde2ebfd2f2f4dbb3a4
selzehairy <selzehairy@gmail.com>
2022-12-14 13:47:51 -0800

    11-print_to_98.c

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -18,7 +18,9 @@ void print_to_98(int n)
 				printf("%d, ", i);
 			else if (i == 98)
 				printf("%d\n", i);
-		}else if (n >= 98)
+		}
+	}
+		else if (n >= 98)
 		{
 			for (j = n; j >= 98; j--)
 			{
@@ -28,5 +30,4 @@ void print_to_98(int n)
 					printf("%d\n", j);
 			}
 		}
-	}
 }
