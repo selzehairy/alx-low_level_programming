29a65cda313ff88f0283eefe54ff30dcdae61c15
selzehairy <selzehairy@gmail.com>
2022-12-22 09:11:12 -0800

    7-leet

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,27 +1,27 @@
 #include "main.h"
 
 /**
- *leet - encode into 1337speak
- *@n: input value
- *Return: n value
- */
+*leet - encode into 1337speak
+*@n: input value
+*Return: n value
+*/
 
 char *leet(char *n)
 {
-  int i, j;
+	int i, j;
 
-  char s1[] = "aAeEoOtTlL";
-  char s2[] = "4433007711";
+	char s1[] = "aAeEoOtTlL";
+	char s2[] = "4433007711";
 
-  for (i = 0; n[i] != '\0'; i++)
-    {
-      for (j = 0; j < 10; j++)
+	for (i = 0; n[i] != '\0'; i++)
 	{
-	  if (n[i] == s1[j])
-	    {
-	      n[i] = s2[j];
-	    }
+		for (j = 0; j < 10; j++)
+	{
+		if (n[i] == s1[j])
+		{
+			n[i] = s2[j];
+		}
 	}
-    }
-  return (n);
+	}
+	return (n);
 }
