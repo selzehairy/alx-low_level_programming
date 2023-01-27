4886350e187655043923d720066ee87a25f4216c
selzehairy <selzehairy@gmail.com>
2023-01-27 06:10:35 -0800

    f3

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -13,16 +13,20 @@ list_t *add_node_end(list_t **head, const char *str)
 	list_t *new, *last;
 
 	new = malloc(sizeof(list_t));
-	if (new==NULL)
+
+	if (new == NULL)
 		 return (NULL);
+
 	dup = strdup(str);
+
 	if (str == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	for (len = 0; str[len];)
+	for (len = 0; str[len]; )
 		len++;
+
 	new->str = dup;
 	new->len = len;
 	new->next = NULL;
